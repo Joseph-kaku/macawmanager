0e5db7c9999189da8486a850a717f889bf50ca62
nfluckiger20 <flu20002@byui.edu>
2023-11-30 14:05:06 -0700

    autho

--- server.ts
+++ server.ts
@@ -17,21 +17,87 @@
 // |                                                                                |
 // \-------------------------------------------------------------------------------*/
 
+// import express, { Request, Response, NextFunction } from 'express';
+// import bodyParser from 'body-parser';
+// import mainRouter from './routes';
+
+// // Port 8080
+// const port: string | number = process.env.PORT || 8080;
+// const app = express();
+// const { auth, requiresAuth } = require('express-openid-connect');
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const config = {
+//   authRequired: false,
+//   auth0Logout: true,
+//   secret: 'a long, randomly-generated string stored in env',
+//   baseURL: 'http://localhost:8080',
+//   clientID: 'tsS7rm0pRY2SejsKP3aNSmEW7zEExYw8',
+//   issuerBaseURL: 'https://dev-2of2wzj2x83rpvs7.us.auth0.com'
+// };
+
+// // auth router attaches /login, /logout, and /callback routes to the baseURL
+// app.use(auth(config));
+
+// // req.isAuthenticated is provided from the auth router
+// app.get('/', (req: Request, res: Response) => {
+//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+// });
+
+// app
+//   .use(bodyParser.json())
+//   .use((req: Request, res: Response, next: NextFunction) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     next();
+//   })
+//   .use('/', mainRouter);  
+// import db from './db';
+// db.mongoose.connect(db.url, {
+// })
+// .then(() => {
+//   app.listen(port, () => {
+//     console.log(`DB Connected and server running on ${port}.`);
+//   });
+// })
+// .catch((err: Error) => { 
+//   console.log('Cannot connect to the database!', err);
+//   process.exit();
+// });
+
+
+// CODE BELOW SHOULD WORK
+
 import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
-import mainRouter from './routes';
-
+import baseRouter from './routes';
+import {auth, requiresAuth} from 'express-openid-connect';
+import dotenv from 'dotenv';
+ 
+dotenv.config();
+ 
 // Port 8080
 const port: string | number = process.env.PORT || 8080;
 const app = express();
-
+ 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET as string,
+  baseURL: process.env.BASE_URL as string,
+  clientID: process.env.CLIENT_ID as string,
+  issuerBaseURL: process.env.ISSUER_BASE_URL as string,
+};
+ 
 app
+  .use(auth(config))
   .use(bodyParser.json())
   .use((req: Request, res: Response, next: NextFunction) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   })
-  .use('/', mainRouter);  
+  .use('/', baseRouter);  
 import db from './db';
 db.mongoose.connect(db.url, {
 })
@@ -43,4 +109,16 @@ db.mongoose.connect(db.url, {
 .catch((err: Error) => { 
   console.log('Cannot connect to the database!', err);
   process.exit();
+});
+ 
+app.get('/', (req: Request, res: Response) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+ 
+app.get('/contacts', requiresAuth(), (req: Request, res: Response) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+ 
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
