df41b8b038091a15b3382db20c334c68001ac1b4
Ryker Swensen <rykerswensen@gmail.com>
2023-12-02 18:52:09 -0700

    RS #24: Final Project Submission

--- tests/delete.spec.ts
+++ tests/delete.spec.ts
@@ -10,22 +10,22 @@ jest.setTimeout(60000);
 
 describe('DELETE Test Handlers', () => {
     test('responds to /completedprojects', async () => {
-        const res = await request.delete('/completedprojects/656ae16f12c69b653667b7a9');
+        const res = await request.delete('/completedprojects/656bc787f94c2a0dc3139a9b');
         expect(res.statusCode).toBe(200)
     });
 
     test('responds to /contacts', async () => {
-        const res = await request.delete('/contacts/656ae17512c69b653667b7ac');
+        const res = await request.delete('/contacts/656bdbda97ff54c35cc95b9a');
         expect(res.statusCode).toBe(404)
     });
 
     test('responds to /projects', async () => {
-        const res = await request.delete('/projects/656ae17512c69b653667b7ae');
+        const res = await request.delete('/projects/656aed8bb3491f9e1385cbf5');
         expect(res.statusCode).toBe(200)
     });
 
     test('responds to /teams', async () => {
-        const res = await request.delete('/teams/656ae17512c69b653667b7b0');
-        expect(res.statusCode).toBe(404)
+        const res = await request.delete('/teams/656ae29da108b3a9cc7f7883');
+        expect(res.statusCode).toBe(200)
     });
 })
